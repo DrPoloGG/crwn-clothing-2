ce49a0e1db4eeaf7677e358ce83237fc27020b99
DrPoloGG <hipolito.garciagracia@gmail.com>
2023-05-26 22:55:53 -0600

    finished checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router';
 
 import Button from '../button/button.component';
 
@@ -8,15 +9,21 @@ import './cart-dropdown.styles.scss';
 import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = () => {
-const { cartItems } = useContext(CartContext);
+  const { cartItems, isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+    setIsCartOpen(!isCartOpen);
+  };
 
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
-        { 
-          cartItems.map(item => <CartItem key={ item.id } cartItem={ item } />)
-        }
-        <Button>CHECKOUT</Button>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+        <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
       </div>
     </div>
   );
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -5,36 +5,88 @@ export const CartContext = createContext({
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
-  cartCount: 0
+  removeItemFromCart: () => {},
+  cartCount: 0,
+  cartTotal: 0,
 });
 
 const addCartItem = (cartItems, productToAdd) => {
   const cartItemFound = cartItems.find(
-    cartItem => cartItem.id === productToAdd.id
+    (cartItem) => cartItem.id === productToAdd.id
   );
 
   if (cartItemFound) {
-    return cartItems.map((cartItem) => 
-      cartItem.id === productToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
     );
   }
 
-  return [ ...cartItems, { ...productToAdd, quantity: 1 }];
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  const cartItemFound = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  if (cartItemFound.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+
+const clearCartItem = (cartItems, cartItemToClear) => {
+  const cartItemFound = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToClear.id
+  );
+
+  if (cartItemFound) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+  }
 };
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
 
-  useEffect( () => {
-    const newCartCount = cartItems.reduce( (total, cartItem) => total + cartItem.quantity, 0 );
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
     setCartCount(newCartCount);
-  },[cartItems])
+  }, [cartItems]);
+  
+
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce(
+      (cartTotal, cartItem) => cartTotal + cartItem.price * cartItem.quantity,
+      0
+    );
+    setCartTotal(newCartTotal);
+  }, [cartItems]);
 
   const addItemToCart = (productToAdd) => {
     const newCartItems = addCartItem(cartItems, productToAdd);
+    setCartItems(newCartItems);
+  };
 
+  const removeItemFromCart = (cartItemToRemove) => {
+    const newCartItems = removeCartItem(cartItems, cartItemToRemove);
+    setCartItems(newCartItems);
+  };
+
+  const clearItemFromCart = (cartItemToClear) => {
+    const newCartItems = clearCartItem(cartItems, cartItemToClear);
     setCartItems(newCartItems);
   };
 
@@ -44,7 +96,10 @@ export const CartProvider = ({ children }) => {
     cartItems,
     setCartItems,
     addItemToCart,
-    cartCount
+    removeItemFromCart,
+    clearItemFromCart,
+    cartCount,
+    cartTotal,
   };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
